bc1c2507f1848a98452f4598b5c00411684e24b0
Brain Atoms <badams6453@gmail.com>
2023-04-29 20:03:00 -0400

    added models and route handling as well as a login page

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,9 @@
+const path = require('path');
+const routes = require('./controllers');
 const express = require("express");
 const session = require("express-session");
-const routes = require("./controllers");
+const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
 
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
@@ -10,7 +13,12 @@ const PORT = process.env.PORT || 3001;
 
 const sess = {
   secret: "Super secret secret",
-  cookie: {},
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict'
+  },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
@@ -20,11 +28,21 @@ const sess = {
 
 app.use(session(sess));
 
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use(require('./controllers'));
 
 app.use(routes);
 
+
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log("Now listening"));
+  app.listen(PORT, () => console.log(`Now listening on ${PORT}`));
 });
