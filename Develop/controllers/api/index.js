8d793b97d1581dfa9f9e35d43874d9cc5cdac5e3
Brain Atoms <badams6453@gmail.com>
2023-05-02 00:56:18 -0400

    updated authorization and made possible to create and delete posts

--- Develop/controllers/api/index.js
+++ Develop/controllers/api/index.js
@@ -1,11 +1,8 @@
 const router = require('express').Router();
-
-const userRoutes = require('./user-routes.js');
-const postRoutes = require('./post-routes');
-const commentRoutes = require('./comment-routes');
+const userRoutes = require('./userRoutes');
+const postRoutes = require('./postRoutes');
 
 router.use('/users', userRoutes);
-router.use('/post', postRoutes);
-router.use('/comment', commentRoutes);
+router.use('/posts', postRoutes);
 
-module.exports = router; 
+module.exports = router;
