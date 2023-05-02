5982ce7a324f9141cd199706098f07db0115473d
Brain Atoms <badams6453@gmail.com>
2023-05-02 16:12:41 -0400

    altered css to change website appearance

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,6 +1,5 @@
 const User = require("./User");
 const Post = require("./Post");
-const Comment = require("./Comment")
 
 User.hasMany(Post, {
   foreignKey: "user_id",
@@ -11,20 +10,4 @@ Post.belongsTo(User, {
   foreignKey: "user_id",
 });
 
-Post.hasMany(Comment, {
-  foreignKey: 'post_id',
-});
-
-Comment.belongsTo(Post, {
-  foreignKey: 'post_id',
-});
-
-Comment.belongsTo(User, {
-  foreignKey: 'user_id',
-});
-
-User.hasMany(Comment, {
-  foreignKey: 'user_id',
-});
-
-module.exports = { User, Post, Comment };
+module.exports = { User, Post };
